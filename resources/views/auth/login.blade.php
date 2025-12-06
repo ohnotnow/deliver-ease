3b102dcd82acea424e284b5eac9bbb02554d9552
Ohffs <ohffsnotnow@gmail.com>
2025-12-06 15:10:27 +0000

    WIP

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,22 @@
-<x-layouts.app>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>{{ config('app.name') }}</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @fluxAppearance
+        @livewireStyles
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
+    </head>
+    <body class="min-h-screen bg-white dark:bg-zinc-800">
 <div class="flex min-h-screen">
     <div class="flex-1 flex justify-center items-center">
         <div class="w-80 max-w-80 space-y-6">
-            <div class="flex justify-center">
-                <flux:heading size="xl">DeliverEase</flux:heading>
-            </div>
+            <img src="/images/splash-mobile.jpg" alt="DeliverEase" class="rounded-lg lg:hidden" />
+
+            <flux:heading class="text-center hidden lg:block" size="xl">DeliverEase</flux:heading>
 
             <flux:heading class="text-center" size="lg">Welcome back</flux:heading>
 
@@ -40,8 +52,10 @@
     </div>
 
     <div class="flex-1 p-4 max-lg:hidden">
-        <div class="relative rounded-lg h-full w-full bg-gradient-to-br from-blue-600 to-indigo-800 flex flex-col items-start justify-end p-16">
-            <div class="text-white">
+        <div class="relative rounded-lg h-full w-full overflow-hidden flex flex-col items-start justify-end p-16">
+            <img src="/images/splash.jpg" alt="" class="absolute inset-0 w-full h-full object-cover" />
+            <div class="absolute inset-0 bg-gradient-to-t from-black/70 via-black/20 to-transparent"></div>
+            <div class="relative text-white">
                 <div class="mb-6 text-3xl xl:text-4xl font-semibold">
                     Keep your customers informed about their deliveries
                 </div>
@@ -53,4 +67,7 @@
         </div>
     </div>
 </div>
-</x-layouts.app>
+        @fluxScripts
+        @stack('scripts')
+    </body>
+</html>
